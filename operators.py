2bc173a01d6fa6f42bc523f5ec6cf592619278a8
vijaychintu <agurlavijaykumar21@gmail.com>
2025-07-09 08:54:50 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,91 @@
+# Arithmetic operators
+num1=3
+num2=2
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+print(num1//num2)
+print(num1**num2)
+
+num = 10
+#num += 5
+#num -= 5
+num *= 5
+print(num)
+
+num1 = 3
+num2 = 2
+isEqual = num1 == num2
+isnotEqual = num1 != num2
+isgreatEqual = num1 >= num2
+
+print(isEqual)
+print(isnotEqual)
+print(isgreatEqual)
+str1 = "python"
+str2 = "Django"
+str3 = "Django"
+str4 = "PYTHON"
+str5 = "PYTHOn"
+
+print(str1 == str2)
+print(str2 == str3)
+print(str1 > str2)
+print(str4 > str5)
+
+num1 = 7
+num2 = 5
+num3 = 3
+num4 = 2
+resultAnd = num1 > num2 and num3 > num4
+resultnot = num1 > num2 
+print(resultAnd)
+print(not resultnot)
+
+text = "python is a programming language"
+isZpresent = 'z' in text
+islpresent = 'l' in text
+ispatternpresent = 'prog' in text
+isjavapresent ='java' not in text
+print(isZpresent)
+print(islpresent)
+print(ispatternpresent)
+print(isjavapresent)
+
+course_list =["java","python","cloud","devops"]
+inpythonPresent = 'python' in course_list
+print(inpythonPresent)
+
+num1 = 10
+num2 = 10
+list1 =[1,2,3]
+list2=[1,2,3]
+print(num1 is num2)
+print(num1 == num2)
+print(list1 is list2)
+print(list1 == list2)
+
+a = 5
+b = 3
+result = a & b
+print(result)
+
+result = a | b
+print(result)
+
+result = a ^ b
+print(result)
+
+
+result - ~ b
+print(result)
+
+print(3<<2)
+print(3<<1)
+print(3<<4)
+print(3>>1)
+print(3>>3)
+print(8>>2)
+
