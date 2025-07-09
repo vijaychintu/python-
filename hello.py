d75bf8d16ba4aa556be7484f56d713c0ce147e2c
vijaychintu <agurlavijaykumar21@gmail.com>
2025-07-09 14:23:20 +0530

    monthly emi

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello git")
+p = float(input("Enter the loan amount: "))
+r = float(input("Enter the annual interest rate (%): "))
+n = int(input("Enter the tenure in years: "))
+
+monthly_rate = r / (12 * 100)
+tenure_months = n * 12
+
+emi = (p * monthly_rate * (1 + monthly_rate) ** tenure_months) / \
+      ((1 + monthly_rate) ** tenure_months - 1)
+
+print(emi)
+
+
