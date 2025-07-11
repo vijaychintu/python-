daa412919f41df04d8405b1b0ace89d8001cc144
vijaychintu <agurlavijaykumar21@gmail.com>
2025-07-11 10:34:23 +0530

    type_conversions

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+# simple conditionals check ----> Boolean
+# 5 > 2 ---> True
+# 2 > 5 ----> False
+if 5>2:
+    print("5 is greater than 2")
+    print("next statement")
---
+++ type_conversions.py
@@ -0,0 +1,37 @@
+a = 100
+b = 3.5
+c = a+b
+print(c)
+print(type(c))
+
+
+x = 100
+y = float(x)
+print(x)
+print(y)
+print(type(y))
+
+
+l = 3.14
+m = int(l)
+print(l)
+print(m)
+print(type(m))
+
+
+x = "100"
+y = int(x)+10
+print(y)
+print(type(y))
+
+z = 10
+num_text = str(z)
+print(type(num_text))
+
+
+
+
+
+
+
+
